121df210f730059dfc81bc0be3a5cf7bea77cb53
Scott <sco.suen.lab@gmail.com>
2016-07-04 12:09:31 -0400

    Feat: change UserController to AsyncController

--- springboot-test/src/main/java/com/ying/springboot_test/controller/AsyncTestController.java
+++ springboot-test/src/main/java/com/ying/springboot_test/controller/AsyncTestController.java
@@ -15,81 +15,99 @@ import org.springframework.web.context.request.async.WebAsyncTask;
 import com.ying.springboot_test.service.UserService;
 
 @RestController
-@RequestMapping(value = "/user/")
-public class UserController {
-	
+@RequestMapping(value = "/async/")
+public class AsyncTestController {
+
 	@Autowired
 	private UserService userService;
-	
-	@RequestMapping(value = "/sign_in/{user_id}", method = RequestMethod.GET)
-	public String signIn (@PathVariable("user_id") Long userId) {
+
+	/**
+	 * normal way
+	 * @param userId
+	 * @return
+	 */
+	@RequestMapping(value = "/1/{user_id}", method = RequestMethod.GET)
+	public String signIn(@PathVariable("user_id") Long userId) {
 		return userService.signIn(userId);
 	}
-	
-	@RequestMapping(value = "/sign_in_2/{user_id}", method = RequestMethod.GET)
-	public Callable<String> signIn2 (@PathVariable ("user_id") Long userId) {
+
+	/**
+	 * return callable, not recommend 
+	 * @param userId
+	 * @return
+	 */
+	@RequestMapping(value = "/2/{user_id}", method = RequestMethod.GET)
+	public Callable<String> signIn2(@PathVariable("user_id") Long userId) {
 		System.out.println("callable start;");
 		Callable<String> callable = new Callable<String>() {
 			@Override
 			public String call() throws Exception {
 				TimeUnit.SECONDS.sleep(5);
-				return "user: " + userId +" sign in success.";
+				return "user: " + userId + " sign in success.";
 			}
 		};
-		
+
 		System.out.println("callable end;");
-		
-		return callable; 
+
+		return callable;
 	}
-	
-	@RequestMapping(value = "/sign_in_3/{user_id}", method = RequestMethod.GET)
-	public DeferredResult<String> signIn3 (@PathVariable("user_id") Long userId) {
+
+	/**
+	 * return deferredResult.
+	 * @param userId
+	 * @return
+	 */
+	@RequestMapping(value = "/3/{user_id}", method = RequestMethod.GET)
+	public DeferredResult<String> signIn3(@PathVariable("user_id") Long userId) {
 		System.out.println("DeferredResult start;");
-		DeferredResult<String> deferredResult = new DeferredResult<>(2000L); //time out time
-		
-		CompletableFuture.<String>supplyAsync(()->{
+		DeferredResult<String> deferredResult = new DeferredResult<>(2000L); // time out
+
+		CompletableFuture.<String> supplyAsync(() -> {
 			try {
 				TimeUnit.SECONDS.sleep(5);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			return "user: " + userId +" sign in success.";
-		}).whenCompleteAsync((result, throwable)->{
+			return "user: " + userId + " sign in success.";
+		}).whenCompleteAsync((result, throwable) -> {
 			deferredResult.setResult(result);
 		});
-		
-		deferredResult.onTimeout(()->{
+
+		deferredResult.onTimeout(() -> {
 			deferredResult.setResult("DeferredResult time out.");
 		});
-		
+
 		System.out.println("DeferredResult end;");
-		
+
 		return deferredResult;
 	}
-	
-	@RequestMapping(value = "/sign_in_4/{user_id}", method = RequestMethod.GET)
-	public WebAsyncTask signIn4 (@PathVariable("user_id") Long userId) {
+
+	/**
+	 * return WebAsyncTask
+	 * @param userId
+	 * @return
+	 */
+	@RequestMapping(value = "/4/{user_id}", method = RequestMethod.GET)
+	public WebAsyncTask signIn4(@PathVariable("user_id") Long userId) {
 		System.out.println("WebAsyncTask start;");
 		Callable<String> callable = new Callable<String>() {
 			@Override
 			public String call() throws Exception {
 				TimeUnit.SECONDS.sleep(5);
-				return "user: " + userId +" sign in success.";
+				return "user: " + userId + " sign in success.";
 			}
 		};
-		
-		WebAsyncTask asyncTask = new WebAsyncTask(2000, callable);
-	    asyncTask.onTimeout(
-	            new Callable<String>() {
-	                public String call() throws Exception {
-	                    return "time out!";
-	                }
-	            }
-	    );
-		
+
+		//new WebAsyncTask<String>(callable);
+		WebAsyncTask asyncTask = new WebAsyncTask(2000, callable); // time out
+		asyncTask.onTimeout(new Callable<String>() {
+			public String call() throws Exception {
+				return "time out!";
+			}
+		});
+
 		System.out.println("WebAsyncTask end;");
-		
-//		return new WebAsyncTask<String>(callable);
+
 		return asyncTask;
 	}
 
